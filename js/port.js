0aa5c91f3e2ea6cd35ef6cb259d639b37013445d
Duong Thanh Truong <dttruonga8tqtpy@gmail.com>
2024-03-07 17:30:32 +0700

    update

--- js/port.js
+++ js/port.js
@@ -1,6 +1,8 @@
 const menu=document.querySelector(".nav-links")
 const menuButton=document.querySelector(".nav-icons")
 const overlay=document.querySelector("#overlay")
+const view_more=document.querySelector(".banner-btn")
+const view_value=document.querySelector(".view-more")
 menuButton.addEventListener('click', ()=>{
     menu.classList.toggle("nav-open");
     menuButton.classList.toggle("open");
@@ -10,4 +12,8 @@ overlay.addEventListener('click',()=>{
     menu.classList.toggle("nav-open");
     menuButton.classList.toggle("open");
     overlay.classList.toggle("show");
+})
+view_more.addEventListener('click',(event)=>{
+    view_value.classList.toggle("visible");
+    event.preventDefault();
 })
