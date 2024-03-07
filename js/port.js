1a4dbb994df03c7a42bbc3d853b5ffd477ab426a
Duong Thanh Truong <dttruonga8tqtpy@gmail.com>
2024-03-07 18:41:26 +0700

    update

--- js/port.js
+++ js/port.js
@@ -17,3 +17,21 @@ view_more.addEventListener('click',(event)=>{
     view_value.classList.toggle("visible");
     event.preventDefault();
 })
+
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    var scrollTopButton = document.getElementById("scrollTopBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollTopButton.style.display = "block";
+    } else {
+        scrollTopButton.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
