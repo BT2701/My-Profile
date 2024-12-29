636d5451400a2fd608186f07cdefe70521205a78
Duong Thanh Truong <truongnopro1111@gmail.com>
2024-12-29 14:43:33 +0700

    update

--- js/port.js
+++ js/port.js
@@ -110,7 +110,7 @@ document.addEventListener("DOMContentLoaded", function () {
 // Hàm kiểm tra phần tử có trong viewport không
 function isInViewport(element) {
     const rect = element.getBoundingClientRect();
-    return rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight);
+    return rect.top < (window.innerHeight || document.documentElement.clientHeight) && rect.bottom >= 0;
 }
 
 // Hàm xác định cấp bậc
